d11e4f8f69df4073d576aca1c7a253d09fdabeaa
YdrMaster <ydrml@hotmail.com>
2022-12-28 16:46:58 +0800

    update: 算了不用 log 了
    Signed-off-by: YdrMaster <ydrml@hotmail.com>

--- try-stm32/src/main.rs
+++ try-stm32/src/main.rs
@@ -3,9 +3,48 @@
 
 mod isr;
 
-extern crate rcore_console;
+use core::fmt::{self, Write};
 
-use rcore_console::{init_console, println};
+/// 打印。
+///
+/// 给宏用的，用户不会直接调它。
+#[doc(hidden)]
+#[inline]
+pub fn _print(args: fmt::Arguments) {
+    Logger.write_fmt(args).unwrap();
+}
+
+/// 格式化打印。
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::_print(core::format_args!($($arg)*));
+    }
+}
+
+/// 格式化打印并换行。
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => {{
+        $crate::_print(core::format_args!($($arg)*));
+        $crate::println!();
+    }}
+}
+
+/// 这个 Unit struct 是 `core::fmt` 要求的。
+struct Logger;
+
+/// 实现 [`Write`] trait，格式化的基础。
+impl Write for Logger {
+    #[inline]
+    fn write_str(&mut self, s: &str) -> Result<(), fmt::Error> {
+        for c in s.bytes() {
+            unsafe { (0x4000C000 as *mut u32).write_volatile(c as _) };
+        }
+        Ok(())
+    }
+}
 
 static mut HELLO: [u8; 13] = *b"Hello, world!";
 
@@ -19,25 +58,6 @@ extern "C" {
 
 #[inline(never)]
 fn main() -> ! {
-    init_console(&Console);
-    unsafe {
-        println!("data   {:?}", &_data as *const _);
-        println!("edata  {:?}", &_edata as *const _);
-        println!("bss    {:?}", &_bss as *const _);
-        println!("ebss   {:?}", &_ebss as *const _);
-        println!("sidata {:?}", &_sidata as *const _);
-
-        let data_len = &_edata as *const u8 as usize - &_data as *const u8 as usize;
-        for b in core::slice::from_raw_parts(&_sidata as *const _ as *const u8, data_len) {
-            println!("{:?} {} {}", b as *const _, b, *b as char);
-        }
-        println!("-------------------------");
-        for b in core::slice::from_raw_parts(&_data as *const _ as *const u8, data_len) {
-            println!("{:?} {} {}", b as *const _, b, *b as char);
-        }
-    }
-    rcore_console::test_log();
-
     loop {
         core::hint::spin_loop();
     }
@@ -67,13 +87,4 @@ extern "C" fn nmi_handler() -> ! {
 #[panic_handler]
 fn panic(_: &core::panic::PanicInfo) -> ! {
     nmi_handler()
-}
-
-struct Console;
-
-impl rcore_console::Console for Console {
-    #[inline]
-    fn put_char(&self, c: u8) {
-        unsafe { (0x4000C000 as *mut u32).write_volatile(c as _) };
-    }
 }
